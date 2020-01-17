f9a9c1c9779422b0297a1a558150e2954715d653
J <T>
2020-01-17 13:11:52 -0500

    Add StockItemController

---
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -0,0 +1,18 @@
+package com.ibm.inventory_management.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StockItemController {
+
+    @GetMapping(path = "/stock-items", produces = "application/json")
+    public List listStockItems() {
+        List items = new ArrayList();
+
+        return items;
+    }
+}
