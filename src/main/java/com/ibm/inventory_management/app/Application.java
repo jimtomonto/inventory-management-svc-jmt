0f214d275f84490f4770cca90accb06ea63186e7
J <T>
2020-01-17 12:45:58 -0500

    Remove hello service and update README.md

--- src/main/java/com/ibm/inventory_management/app/Application.java
+++ src/main/java/com/ibm/inventory_management/app/Application.java
@@ -1,4 +1,4 @@
-package application.app;
+package com.ibm.inventory_management.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,20 +12,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@ComponentScan({"com.ibm.hello.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
+@ComponentScan({ "com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health" })
 public class Application extends SpringBootServletInitializer {
     @Autowired
     Environment environment;
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
     }
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             String port = environment.getProperty("local.server.port");
-
             System.out.println();
             System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
         };
@@ -35,5 +34,4 @@ public class Application extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
-
 }
