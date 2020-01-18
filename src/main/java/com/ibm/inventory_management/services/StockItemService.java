5f189eeb6978a74a2c943870689386d1412b3ada
J <T>
2020-01-18 18:41:25 -0500

    Add a few more items to the inventory

--- src/main/java/com/ibm/inventory_management/services/StockItemService.java
+++ src/main/java/com/ibm/inventory_management/services/StockItemService.java
@@ -14,6 +14,8 @@ public class StockItemService implements StockItemApi {
     public List<StockItem> listStockItems() {
         return asList(new StockItem("1").withName("Item 1").withStock(100).withPrice(10.5).withManufacturer("Sony"),
                 new StockItem("2").withName("Item 2").withStock(150).withPrice(100.0).withManufacturer("Insignia"),
-                new StockItem("3").withName("Item 3").withStock(10).withPrice(1000.0).withManufacturer("Panasonic"));
+                new StockItem("3").withName("Item 3").withStock(10).withPrice(1000.0).withManufacturer("Panasonic"),
+                new StockItem("4").withName("Item 4").withStock(10).withPrice(1000.0).withManufacturer("Apple"),
+                new StockItem("5").withName("Item 5").withStock(10).withPrice(1000.0).withManufacturer("Microsoft"));
     }
 }
